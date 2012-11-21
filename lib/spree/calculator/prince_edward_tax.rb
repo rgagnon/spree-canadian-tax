e710db6d9fbc0594ac5c5ed75447798b6c5fc47e
Jorge Barnaby <jorge.barnaby@gmail.com>
2012-11-20 21:34:46 -0500

    Prince Edward Island tax calculation added.

---
+++ lib/spree/calculator/prince_edward_tax.rb
@@ -0,0 +1,26 @@
+class Spree::Calculator::PrinceEdwardTax < Spree::Calculator 
+  def self.description
+    "Prince Edward Tax"
+  end
+
+  def self.register
+    super
+  end
+
+  def compute(order)
+    calculate_taxation(order.item_total)
+  end
+
+  private
+
+  def calculate_taxation(total)
+    tps = total * 0.05
+    tvq = (total + tps) * 0.1 #TVQ
+    tax = round_to_two_places(tps + tvq)
+    return tax
+  end
+
+  def round_to_two_places(amount)
+    BigDecimal.new(amount.to_s).round(2, BigDecimal::ROUND_HALF_UP)
+  end
+end
